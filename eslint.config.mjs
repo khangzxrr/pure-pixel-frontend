b690d43be7ffa78062791ad279b284cadacab614
khangzxrr <khangzxrr@gmail.com>
2024-08-26 19:36:48 +0700

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import checkFile from 'eslint-plugin-check-file';
+
+export default [
+  {
+    files: ['src/**/*'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "camelcase": "warn",
+      'check-file/folder-naming-convention': ['error', { 'src/**/': 'CAMEL_CASE' }],
+      'check-file/filename-naming-convention': ['error', { 'src/**/**': 'PASCAL_CASE' }],
+    },
+  },
+  
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  
+];
+
