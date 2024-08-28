80038dbbc2f78a6e3397c6d6becbc2a6a11c935a
khangzxrr <khangzxrr@gmail.com>
2024-08-28 16:22:51 +0700

    import React when using jsx

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-
+import React from "react";
 const MainLayout = () => {
   return (
     <>
