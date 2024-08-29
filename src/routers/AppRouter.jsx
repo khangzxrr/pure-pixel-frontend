df3e15e10117464fd124b8933c6a1987133a2b51
TrungNguyenHRZ <trungnt10012k@gmail.com>
2024-08-29 21:15:32 +0700

    add for you page

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,11 +2,35 @@ import { createBrowserRouter } from "react-router-dom";
 import AuthorizedLayout from "../layouts/AuthorizedLayout";
 import MainLayout from "../layouts/MainLayout";
 import ProfilePage from "../pages/Profile/Profile";
+import Dashboard from "../components/Dashboard/Dashboard";
+import ForYou from "../components/Dashboard/ForYou/ForYou";
+import Following from "../components/Dashboard/Following/Following";
+import Explore from "../components/Dashboard/Explore/Explore";
 
 export const AppRouter = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+        children: [
+          {
+            path: "/following",
+            element: <Following />,
+          },
+          {
+            path: "/for-you",
+            element: <ForYou />,
+          },
+          {
+            path: "/explore",
+            element: <Explore />,
+          },
+        ],
+      },
+    ],
   },
   {
     path: "/home",
