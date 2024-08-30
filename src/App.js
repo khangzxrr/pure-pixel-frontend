c304bb27ac26f05a5293423b323b51cad84c96c0
khangzxrr <khangzxrr@gmail.com>
2024-08-31 02:10:09 +0700

    upgrade react-query to tanstack-query v5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloakService from "./services/Keycloak";
 import { AppRouter } from "./routers/AppRouter";
 import { RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+    },
+  },
+});
 function App() {
   return (
-    <ReactKeycloakProvider authClient={keycloakService}>
-      <RouterProvider router={AppRouter} />
-    </ReactKeycloakProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactKeycloakProvider authClient={keycloakService}>
+        <ToastContainer />
+        <RouterProvider router={AppRouter} />
+        <ReactQueryDevtools buttonPosition="bottom-right" />
+      </ReactKeycloakProvider>
+    </QueryClientProvider>
   );
 }
 
