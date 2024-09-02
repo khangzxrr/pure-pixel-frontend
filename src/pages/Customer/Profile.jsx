f87b949033731fa0c5bb1dd7321c30dcc4406251
khangzxrr <khangzxrr@gmail.com>
2024-09-03 01:12:38 +0700

    fetch profile API

--- src/pages/Customer/Profile.jsx
+++ src/pages/Customer/Profile.jsx
@@ -1,14 +1,31 @@
-import { useKeycloak } from "@react-keycloak/web";
-import { useEffect, useState } from "react";
+import { useQueries } from "@tanstack/react-query";
+import UserApi from "../../apis/UserApi";
 
 export default function Profile() {
-  const { keycloak } = useKeycloak();
-  const [profile, setProfile] = useState({});
-  useEffect(() => {
-    keycloak.loadUserProfile().then((profile) => {
-      console.log(profile);
-      setProfile(profile);
-    });
-  }, []);
-  return <div>Hello {JSON.stringify(profile)}</div>;
+  const result = useQueries({
+    queries: [
+      {
+        queryKey: ["keycloak", 1],
+        queryFn: UserApi.getKeycloakProfile,
+      },
+      {
+        queryKey: ["application", 2],
+        queryFn: UserApi.getApplicationProfile,
+      },
+    ],
+  });
+
+  if (result[0].isLoading) {
+    return <div>Loading keycloak profile...</div>;
+  }
+
+  if (result[1].isLoading) {
+    return <div>Loading application profile... </div>;
+  }
+
+  return (
+    <div>
+      {JSON.stringify(result[0].data)} {JSON.stringify(result[1].data)}
+    </div>
+  );
 }
