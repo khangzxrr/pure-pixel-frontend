62db747353a77d4fbcabf322fb136f084eae493c
H <110007694+hohongminh@users.noreply.github.com>
2024-10-03 21:14:47 +0700

    call api share

--- src/components/ComSharePhoto/ComSharePhoto.jsx
+++ src/components/ComSharePhoto/ComSharePhoto.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { User, Link, ChevronDown, Info } from "lucide-react";
-import { Select } from "antd";
+import { Select, Skeleton } from "antd";
 import PhotoApi from "../../apis/PhotoApi";
 import { useQuery } from "@tanstack/react-query";
 import { FaClipboard } from "react-icons/fa";
 import { useNotification } from "./../../Notification/Notification";
+import { useState } from "react";
+import { getData, postData } from "./../../apis/api";
 export default function ComSharePhoto({ idImg, onClose }) {
   const { notificationApi } = useNotification();
+  const [availableResolution, setAvailableResolution] = useState([]);
   const handleChange = (value) => {
     console.log(`selected ${value}`);
+    callApiShare(value);
   };
+  const callApiShare = (value) => {
+    postData(`/photo/share`, {
+      photoId: idImg,
+      resolution: value,
+    })
+      .then((e) => {
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    try {
+      callApiShare(resolutionOptions[0]?.value);
+    } catch (error) {}
+  }, []);
   const AvailableResolutions = useQuery({
     queryKey: ["getAvailableResolutionsByPhotoId", idImg],
     queryFn: () => PhotoApi.getAvailableResolutionsByPhotoId(idImg),
   });
+
   console.log(idImg);
   console.log(AvailableResolutions);
 
@@ -22,12 +45,16 @@ export default function ComSharePhoto({ idImg, onClose }) {
     navigator.clipboard
       .writeText(`${baseURL}/photo/${idImg}`)
       .then(() => {
-        notificationApi("success", "Thành công", "Đã copy liên kết");
+        notificationApi("success", "Thành công", "Đã sao chép liên kết");
       })
       .catch((err) => {
         console.error("Failed to copy link: ", err);
       });
   };
+  const resolutionOptions = AvailableResolutions?.data?.map((res) => ({
+    value: res.resolution,
+    label: res.resolution,
+  }));
   return (
     <div className="bg-white text-gray-800 py-8 px-1 max-w-md mx-auto">
       <div className="flex items-center justify-between mb-4">
@@ -36,22 +63,9 @@ export default function ComSharePhoto({ idImg, onClose }) {
           <Info size={20} />
         </button>
       </div>
-
-      {/* <div className="mb-6">
-        <input
-          type="text"
-          placeholder="Add people, groups and calendar events"
-          className="w-full bg-gray-100 text-gray-800 px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-      </div> */}
-
-      {/* <h3 className="text-lg font-semibold mb-2 flex items-center">
-        People with access
-        <button className="ml-2 text-gray-500 hover:text-gray-700">
-          <Info size={16} />
-        </button>
-      </h3> */}
-      {false ? (
+      {AvailableResolutions.isLoading ? (
+        <Skeleton active />
+      ) : AvailableResolutions?.data ? (
         <>
           <h3 className="text-lg font-semibold mb-2">Lựa chọn </h3>
           <div className="flex items-center justify-between bg-gray-100 p-3 rounded-md mb-6">
@@ -61,7 +75,7 @@ export default function ComSharePhoto({ idImg, onClose }) {
             </div>
             <div className="flex items-center">
               <Select
-                defaultValue="Viewer"
+                defaultValue={resolutionOptions[0].value}
                 variant="borderless"
                 style={{
                   width: 120,
@@ -69,10 +83,7 @@ export default function ComSharePhoto({ idImg, onClose }) {
                   textAlign: "end",
                 }}
                 onChange={handleChange}
-                options={[
-                  { value: "Viewer", label: "Viewer" },
-                  { value: "Commenter", label: "Commenter" },
-                ]}
+                options={resolutionOptions}
               />
             </div>
           </div>
@@ -95,7 +106,7 @@ export default function ComSharePhoto({ idImg, onClose }) {
           onClick={copyToClipboard}
           className="bg-gray-200 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500"
         >
-          Copy link
+          Copy đường dẫn
         </button>
         <button
           onClick={onClose}
