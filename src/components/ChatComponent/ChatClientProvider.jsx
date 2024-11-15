7425268b3449753b14c852940c5ec71367cf62e2
Vo Khang <vokhang@192.168.1.4.non-exists.ptr.local>
2024-11-15 16:35:14 +0700

    remove all useEffect

--- src/components/ChatComponent/ChatClientProvider.jsx
+++ src/components/ChatComponent/ChatClientProvider.jsx
@@ -1,57 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Chat } from "stream-chat-react";
+import React, { useState } from "react";
+import { Chat, useCreateChatClient } from "stream-chat-react";
 import ChatApi from "../../apis/ChatApi";
 import "stream-chat-react/dist/css/v2/index.css";
 import "./ChannelInbox.css";
-import { PacmanLoader } from "react-spinners";
 import { useMutation } from "@tanstack/react-query";
-import { StreamChat } from "stream-chat";
 
 export default function ChatClientProvider({ user, children }) {
-  const [token, setToken] = useState(null);
-  const [client, setClient] = useState(null);
-  const [, setIsClientReady] = useState(false);
-
   // Mutation to fetch authentication token
   const { mutate: fetchToken, isLoading: isFetchingToken } = useMutation({
     mutationFn: async () => await ChatApi.auth(),
-    onSuccess: (retrievedToken) => {
-      setToken(retrievedToken);
-    },
     onError: (error) => {
       console.error("Authentication failed:", error);
     },
   });
 
-  // // Trigger token fetch on component mount
-  useEffect(() => {
-    fetchToken();
-  }, []);
+  const client = useCreateChatClient({
+    apiKey: process.env.REACT_APP_STREAM_API_KEY,
+    tokenOrProvider: fetchToken,
+    userData: { id: user.id },
+  });
 
-  // Initialize the chat client only when the token is available
-  useEffect(() => {
-    if (token) {
-      const chatClient = StreamChat.getInstance(
-        process.env.REACT_APP_STREAM_API_KEY,
-      );
-      chatClient
-        .connectUser({ id: user.id, name: user.name }, token)
-        .then(() => {
-          setClient(chatClient);
-          setIsClientReady(true);
-        });
-    }
-  }, [token, user.id, user.name]);
-
-  // Show loading spinner if token is being fetched or client isn't ready
-
-  if (client) {
-    return (
-      <Chat client={client} theme="str-chat__theme-dark">
-        {children}
-      </Chat>
-    );
+  if (!client) {
+    return <div>{children}</div>;
   }
 
-  return <div>{children}</div>;
+  return (
+    <Chat client={client} theme="str-chat__theme-dark">
+      {children}
+    </Chat>
+  );
 }
--- src/components/ChatComponent/ChatProvider.jsx
+++ src/components/ChatComponent/ChatProvider.jsx
@@ -2,27 +2,9 @@ import "stream-chat-react/dist/css/v2/index.css";
 import "./ChannelInbox.css";
 import { useKeycloak } from "@react-keycloak/web";
 import ChatClientProvider from "./ChatClientProvider";
-import { ClipLoader, PacmanLoader } from "react-spinners";
 
 const ChatProvider = ({ children }) => {
-  const { keycloak, initialized } = useKeycloak();
-
-  if (!initialized) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          justifyItems: "center",
-          alignItems: "center",
-          textAlign: "center",
-          minHeight: "100vh",
-        }}
-      >
-        <PacmanLoader />
-      </div>
-    );
-  }
+  const { keycloak } = useKeycloak();
 
   if (keycloak.authenticated && keycloak.tokenParsed) {
     const user = {
