45065b6197c32d92e13e1f8db131c343d952b684
Vo Khang <vokhang@192.168.1.4.non-exists.ptr.local>
2024-11-15 16:45:26 +0700

    remove mutation

--- src/components/ChatComponent/ChatClientProvider.jsx
+++ src/components/ChatComponent/ChatClientProvider.jsx
@@ -1,22 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { Chat, useCreateChatClient } from "stream-chat-react";
 import ChatApi from "../../apis/ChatApi";
 import "stream-chat-react/dist/css/v2/index.css";
 import "./ChannelInbox.css";
-import { useMutation } from "@tanstack/react-query";
 
 export default function ChatClientProvider({ user, children }) {
-  // Mutation to fetch authentication token
-  const { mutate: fetchToken } = useMutation({
-    mutationFn: async () => await ChatApi.auth(),
-    onError: (error) => {
-      console.error("Authentication failed:", error);
-    },
-  });
-
   const client = useCreateChatClient({
     apiKey: process.env.REACT_APP_STREAM_API_KEY,
-    tokenOrProvider: fetchToken,
+    tokenOrProvider: ChatApi.auth,
     userData: { id: user.id },
   });
 
