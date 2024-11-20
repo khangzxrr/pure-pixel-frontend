7a833fa7c51388e213b7939078731ca4ec6e09be
Vo Khang <vokhang@MacBook-Pro-cua-Vo.local>
2024-11-20 13:13:45 +0700

    change to vite env

--- src/components/ChatComponent/ChatClientProvider.jsx
+++ src/components/ChatComponent/ChatClientProvider.jsx
@@ -6,7 +6,7 @@ import "./ChannelInbox.css";
 
 export default function ChatClientProvider({ user, children }) {
   const client = useCreateChatClient({
-    apiKey: process.env.REACT_APP_STREAM_API_KEY,
+    apiKey: import.meta.env.VITE_STREAM_API_KEY,
     tokenOrProvider: ChatApi.auth,
     userData: { id: user.id },
   });
