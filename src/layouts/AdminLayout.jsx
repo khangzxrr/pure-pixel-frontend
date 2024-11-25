7c4f73036b025d73b302cfb5e7e6404679d0f2b6
H <110007694+hohongminh@users.noreply.github.com>
2024-11-25 23:43:26 +0700

    add avata layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -261,10 +261,10 @@ export default function AdminLayout({ children }) {
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-56 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-[#32353b] px-4 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
+            <div className="flex h-16 shrink-0 items-center justify-center">
               <img
                 alt="Your Company"
-                src="/assets/logoP.png"
+                src="/purepixel.png"
                 className="h-8 w-auto"
               />
             </div>
