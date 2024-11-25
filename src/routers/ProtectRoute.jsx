958097cbe09437daa24dcf917179cbb936887dd2
khangzxrr <khangzxrr@gmail.com>
2024-11-25 11:44:38 +0700

    fix

--- src/routers/ProtectRoute.jsx
+++ src/routers/ProtectRoute.jsx
@@ -1,7 +1,5 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
-import UserService from "../services/Keycloak";
-import ErrorPage from "../pages/ErrorPage";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 import { useKeycloak } from "@react-keycloak/web";
 
 //checkRoles bay gio co the nhan 1 mang cac role
@@ -11,17 +9,19 @@ const ProtectRoute = ({ children, checkRoles }) => {
   const roles =
     keycloak.tokenParsed?.resource_access?.[keycloak.clientId]?.roles;
 
-  useEffect(() => {
-    for (let role of roles) {
-      if (checkRoles.includes(role)) {
-        return;
-      }
+  if (!keycloak?.authenticated) {
+    keycloak.login();
+
+    return;
+  }
+
+  for (let role of roles) {
+    if (checkRoles.includes(role)) {
+      return <>{children}</>;
     }
+  }
 
-    navigate("/", { replace: true });
-  }, [roles, checkRoles]);
-
-  return <>{children}</>;
+  navigate("/");
 };
 
 export default ProtectRoute;
