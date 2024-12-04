c9ae3d9a215b38879ba5a930acc8e671afa22703
TrungNguyenHRZ <trungnt10012k@gmail.com>
2024-12-04 22:30:49 +0700

    add authenticate orderByFollowed

--- src/components/Inspiration/InspirationPhoto/InsPhotoFilter.jsx
+++ src/components/Inspiration/InspirationPhoto/InsPhotoFilter.jsx
@@ -3,7 +3,9 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import UseCategoryStore from "../../../states/UseCategoryStore";
 import { IoCaretDownCircleOutline } from "react-icons/io5";
+import { useKeycloak } from "@react-keycloak/web";
 const InsPhotoFilter = () => {
+  const { keycloak } = useKeycloak();
   const {
     isWatermarkChecked,
     isForSaleChecked,
@@ -132,47 +134,50 @@ const InsPhotoFilter = () => {
         </Menu>
       </div>
 
-      <div>
-        <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <MenuButton className="inline-flex items-center w-full justify-center gap-x-1.5 rounded-md   py-2 text-sm font-semibold text-[#eee] ">
-              <span className="font-normal text-[16px] text-[#a3a3a3]">
-                Theo dõi:
-              </span>{" "}
-              <div className="text-[16px]">{filterByFollowed.name || ""}</div>
-              <IoCaretDownCircleOutline className="text-lg" />
-            </MenuButton>
-          </div>
+      {keycloak.authenticated && (
+        <div>
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="inline-flex items-center w-full justify-center gap-x-1.5 rounded-md   py-2 text-sm font-semibold text-[#eee] ">
+                <span className="font-normal text-[16px] text-[#a3a3a3]">
+                  Theo dõi:
+                </span>{" "}
+                <div className="text-[16px]">{filterByFollowed.name || ""}</div>
+                <IoCaretDownCircleOutline className="text-lg" />
+              </MenuButton>
+            </div>
 
-          <MenuItems
-            transition
-            className="absolute left-0 z-10 mt-2 w-40 origin-top rounded-md bg-[#202225] shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-          >
-            <div className="py-1">
-              {filterByFollowedList.map((item) => (
-                <MenuItem key={item.id} className="hover:cursor-pointer">
+            <MenuItems
+              transition
+              className="absolute left-0 z-10 mt-2 w-40 origin-top rounded-md bg-[#202225] shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="py-1">
+                {filterByFollowedList.map((item) => (
+                  <MenuItem key={item.id} className="hover:cursor-pointer">
+                    <div
+                      onClick={() =>
+                        handleFilterByIsFollowed(item.name, item.param)
+                      }
+                      className="block px-4 py-2 text-sm text-[#eee] data-[focus]:bg-[#eee] data-[focus]:text-[#2f3136] transition-colors duration-200 ease-in-out"
+                    >
+                      {item.name}
+                    </div>
+                  </MenuItem>
+                ))}
+                <MenuItem className="hover:cursor-pointer">
                   <div
-                    onClick={() =>
-                      handleFilterByIsFollowed(item.name, item.param)
-                    }
-                    className="block px-4 py-2 text-sm text-[#eee] data-[focus]:bg-[#eee] data-[focus]:text-[#2f3136] transition-colors duration-200 ease-in-out"
+                    onClick={() => handleFilterByIsFollowed("", "")}
+                    className="block px-4 py-2 text-sm text-red-500 data-[focus]:bg-red-500 data-[focus]:text-[#eee] transition-colors duration-200 ease-in-out"
                   >
-                    {item.name}
+                    Xoá bộ lọc
                   </div>
                 </MenuItem>
-              ))}
-              <MenuItem className="hover:cursor-pointer">
-                <div
-                  onClick={() => handleFilterByIsFollowed("", "")}
-                  className="block px-4 py-2 text-sm text-red-500 data-[focus]:bg-red-500 data-[focus]:text-[#eee] transition-colors duration-200 ease-in-out"
-                >
-                  Xoá bộ lọc
-                </div>
-              </MenuItem>
-            </div>
-          </MenuItems>
-        </Menu>
-      </div>
+              </div>
+            </MenuItems>
+          </Menu>
+        </div>
+      )}
+
       {/* filter by up vote menu */}
       {/* <div>
           <Menu as="div" className="relative inline-block text-left">
