7a658cbffab8addbde85f4ad9a0fff1995b6b9d4
H <110007694+hohongminh@users.noreply.github.com>
2024-12-13 18:50:44 +0700

    fix table

--- src/components/ComCard/ComCard.jsx
+++ src/components/ComCard/ComCard.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { Tooltip } from "antd";
+import React, { useState } from "react";
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 
 export default function ComCard({ title, value, icon, onClick, isSelected }) {
+  const [isBalanceVisible, setIsBalanceVisible] = useState(false); // State for visibility
+
   function formatCurrency(number) {
     // Sử dụng hàm toLocaleString() để định dạng số thành chuỗi với ngăn cách hàng nghìn và mặc định là USD.
     if (typeof number === "number") {
@@ -10,31 +14,49 @@ export default function ComCard({ title, value, icon, onClick, isSelected }) {
       });
     }
   }
+  const toggleBalanceVisibility = () => {
+    setIsBalanceVisible(!isBalanceVisible);
+  };
+
   return (
     <div
       onClick={onClick}
-      className={`rounded-lg p-1 border shadow-md flex flex-col items-center cursor-pointer ${
-        isSelected
-          ? "bg-[#0F296D] text-white border-indigo-600" // Viền xanh khi được chọn
-          : "bg-white text-black border-gray-300" // Viền xám khi không được chọn
-      }`}
+      className={`  col-span-1 bg-[#2a2c32] shadow rounded-lg p-3 flex flex-col`}
     >
-      <div className="flex justify-between items-center gap-3">
-        {icon && (
+      <p className="text-lg text-[#b9b3b3]">{title}</p>
+      <div className="flex items-center justify-between w-full cursor-pointer text-[#dddddd] hover:text-white p-2">
+        {isBalanceVisible ? (
           <div
-            className={`rounded-full p-2 ${
-              isSelected ? "bg-white" : "bg-black" // Màu nền icon
-            }`}
+            className="w-full flex flex-row justify-between"
+            onClick={toggleBalanceVisibility}
           >
-            {icon}
+            <div className="w-5/6">
+              {value
+                ? Intl.NumberFormat("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                  }).format(value)
+                : 0}
+            </div>
+            <div className="w-1/6">
+              <Tooltip title="Ẩn số dư">
+                <EyeOutlined />
+              </Tooltip>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="w-full flex flex-row justify-between"
+            onClick={toggleBalanceVisibility}
+          >
+            <div className="w-5/6">******</div>
+            <div className="w-1/6">
+              <Tooltip title="Hiện số dư">
+                <EyeInvisibleOutlined />
+              </Tooltip>
+            </div>
           </div>
         )}
-        <div className="h-14 justify-center  items-center">
-          <div className="font-mono font-bold text-center text-xl">{title}</div>
-          <div className="font-mono font-bold text-center text-xl">
-            {formatCurrency(value)}
-          </div>
-        </div>
       </div>
     </div>
   );
