e310890cdd3ba9e383d8e28b5314628bca09576d
TrungNguyenHRZ <trungnt10012k@gmail.com>
2024-12-16 22:50:37 +0700

    add ban photoshoot package

--- src/pages/UserProfile/MyPhotoshootPackageDetailInfo.jsx
+++ src/pages/UserProfile/MyPhotoshootPackageDetailInfo.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { FaEdit } from "react-icons/fa";
 import formatPrice from "../../utils/FormatPriceUtils";
 import calculateDateDifference from "../../utils/calculateDateDifference";
+import { PiWarningOctagonFill } from "react-icons/pi";
 
 const MyPhotoshootPackageDetailInfo = ({
   photoshootPackage,
   setIsUpdatePhotoshootPackageModal,
   setSelectedUpdatePhotoshootPackage,
 }) => {
+  console.log("photoshootPackage", photoshootPackage);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 rounded-lg gap-5 bg-[#292b2f] overflow-hidden">
       <div className="h-[500px] overflow-hidden">
@@ -26,18 +29,26 @@ const MyPhotoshootPackageDetailInfo = ({
               {photoshootPackage.subtitle}
             </div>
           </div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              setIsUpdatePhotoshootPackageModal(true);
-              setSelectedUpdatePhotoshootPackage(photoshootPackage.id);
-            }}
-            className="font-normal flex items-center gap-2 px-2 py-1 bg-[#19191b] text-[#5699ff] rounded-md hover:opacity-80"
-          >
-            <FaEdit className="size-5" />
-            Chỉnh sửa
-          </button>
+
+          {photoshootPackage?.status === "ENABLED" ? (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsUpdatePhotoshootPackageModal(true);
+                setSelectedUpdatePhotoshootPackage(photoshootPackage.id);
+              }}
+              className="font-normal flex items-center gap-2 px-2 py-1 bg-[#19191b] text-[#5699ff] rounded-md hover:opacity-80"
+            >
+              <FaEdit className="size-5" />
+              Chỉnh sửa
+            </button>
+          ) : (
+            <div className="font-normal flex items-center gap-2 px-2 py-1 bg-red-500 text-white rounded-md ">
+              <PiWarningOctagonFill className="size-5" />
+              Gói dịch vụ này đã bị vô hiệu hóa
+            </div>
+          )}
         </div>
 
         <div className="flex flex-col gap-2">
